0825911378393b027f60114b80a0530ce2cd0ad6
Khushi <khushipanwaegzb@gmail.com>
2024-09-02 01:13:12 +0530

    Initial Commit

---
+++ src/app/layout.js
@@ -0,0 +1,22 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Header from "./components/Header";
+
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "IMdb clone",
+  description: "This is Movie database clone",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+ 
+        <Header />
+        {children}</body>
+    </html>
+  );
+}
