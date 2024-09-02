2f9b5d02b74cfbd6122dcdb32616b8fb28565fec
Khushi <khushipanwaegzb@gmail.com>
2024-09-02 22:00:24 +0530

    handle the error

---
+++ src/app/components/Result.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Result( {results} ) {
+  return (
+    <div>
+      {
+        results && results.map((result) =>(
+          <div key={result.id}>
+            <h2>{result.original_title}</h2>
+            </div>
+        ))
+      }
+    </div>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,23 @@
+const API_KEY = process.env.API_KEY;
 import React from 'react'
+import Result from './components/Result';
 
-export default function page() {
+export default async function page({searchParams}) {
+  const genre = searchParams.genre || 'fetchTrending' ;
+  const res = await fetch(`https://api.themoviedb.org/3${genre === 'fetchTopRated' ?`/movie/top_rated`:`/trending/all/week`}?api_key=${API_KEY}& language=en-US&page=1` ,{
+    next:{revalidate:10000}
+  }
+    );
+    const data = await res.json();
+    if(!res.ok){
+      throw new Error(`Failed to fetch Data`)
+    }
+    const results = data.results;
+    // console.log(results);
+
   return (
     <div className='text-gray-700 dark:text-gray-200 dark:bg-gray-700  select-none transition-colors duration-300 min-h-screen'>
-      HOME
+      <Result results={results} />
     </div>
   )
 }
