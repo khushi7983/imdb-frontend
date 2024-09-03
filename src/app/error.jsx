7f367480e99f090eb4bb8c2ab7706cf437933efa
Khushi <khushipanwaegzb@gmail.com>
2024-09-03 13:11:53 +0530

    update design

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -6,7 +6,7 @@ export default function error({error,reset}) {
     console.log(error);
   }, [error])
   return (
-    <div className='text-center mt-10 text-gray-700 dark:text-gray-200 dark:bg-gray-700  select-none transition-colors duration-300 min-h-screen'>
+    <div className='text-center  text-gray-700 dark:text-gray-200 dark:bg-gray-700  select-none transition-colors duration-300 min-h-screen'>
       <h1>Something went wrong. Please try again later</h1>
       <button  className='hover:text-amber-600'  onClick={(() =>reset())}>Try Again</button>
     </div>
