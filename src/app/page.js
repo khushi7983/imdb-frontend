b0859c8a1bbcd98bcd799fec0140da1b8ad3d728
Khushi Panwar <khushipamwargzb@gmail.com>
2025-04-16 21:26:28 +0530

    fixed

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import Result from './components/Result';
 
 export default async function page({searchParams}) {
   const genre = searchParams.genre || 'fetchTrending' ;
-  const res = await fetch(`https://api.themoviedb.org/3${genre === 'fetchTopRated' ?`/movie/top_rated`:`/trending/all/week`}?api_key=${API_KEY}& language=en-US&page=1` ,{
+  const res = await fetch(`https://api.themoviedb.org/3${genre === 'fetchTopRated' ?`/movie/top_rated`:`/trending/all/week`}?api_key=${API_KEY}&language=en-US&page=1` ,{
     next:{revalidate:10000}
   }
     );
